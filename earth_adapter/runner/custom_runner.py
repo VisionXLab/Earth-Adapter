16308511394eea2b01e684f6f716e47858669ed4
xiaoxinghu <xiaoxinghhh@gmail.com>
2025-04-28 11:39:40 +0800

    bug fix

--- earth_adapter/runner/custom_runner.py
+++ earth_adapter/runner/custom_runner.py
@@ -1,8 +1,14 @@
 from mmengine.runner import Runner
 from mmengine.registry import RUNNERS
-from typing import Callable, Union
-from mmengine.runner.checkpoint import _load_checkpoint,_load_checkpoint_to_model,find_latest_checkpoint
+from typing import Optional
 from mmengine.model import is_model_wrapper
+from mmengine.fileio import FileClient, join_path
+import warnings
+import mmengine
+import time
+from mmengine.utils import apply_to, get_git_hash
+from mmengine.runner.checkpoint import save_checkpoint, weights_to_cpu
+from mmengine.optim import OptimWrapper
 @RUNNERS.register_module()
 class custom_runner(Runner):
 
@@ -29,47 +35,136 @@ class custom_runner(Runner):
         metrics = self.test_loop.run()  # type: ignore
         self.call_hook('after_run')
         return metrics
-    def load_checkpoint(self,
-                        filename: str,
-                        map_location: Union[str, Callable] = 'cpu',
-                        strict: bool = False,
-                        revise_keys: list = [(r'^module.', '')]):
-        """Load checkpoint from given ``filename``.
+    def save_checkpoint(
+        self,
+        out_dir: str,
+        filename: str,
+        file_client_args: Optional[dict] = None,
+        save_optimizer: bool = True,
+        save_param_scheduler: bool = True,
+        meta: Optional[dict] = None,
+        by_epoch: bool = True,
+        backend_args: Optional[dict] = None,
+    ):
+        
+        """Save checkpoints.
+
+        ``CheckpointHook`` invokes this method to save checkpoints
+        periodically.
 
         Args:
-            filename (str): Accept local filepath, URL, ``torchvision://xxx``,
-                ``open-mmlab://xxx``.
-            map_location (str or callable): A string or a callable function to
-                specifying how to remap storage locations.
-                Defaults to 'cpu'.
-            strict (bool): strict (bool): Whether to allow different params for
-                the model and checkpoint.
-            revise_keys (list): A list of customized keywords to modify the
-                state_dict in checkpoint. Each item is a (pattern, replacement)
-                pair of the regular expression operations. Defaults to strip
-                the prefix 'module.' by [(r'^module\\.', '')].
+            out_dir (str): The directory that checkpoints are saved.
+            filename (str): The checkpoint filename.
+            file_client_args (dict, optional): Arguments to instantiate a
+                FileClient. See :class:`mmengine.fileio.FileClient` for
+                details. Defaults to None. It will be deprecated in future.
+                Please use `backend_args` instead.
+            save_optimizer (bool): Whether to save the optimizer to
+                the checkpoint. Defaults to True.
+            save_param_scheduler (bool): Whether to save the param_scheduler
+                to the checkpoint. Defaults to True.
+            meta (dict, optional): The meta information to be saved in the
+                checkpoint. Defaults to None.
+            by_epoch (bool): Decide the number of epoch or iteration saved in
+                checkpoint. Defaults to True.
+            backend_args (dict, optional): Arguments to instantiate the
+                prefix of uri corresponding backend. Defaults to None.
+                New in v0.2.0.
         """
-        checkpoint = _load_checkpoint(filename, map_location=map_location)
-        state_dict = checkpoint['state_dict']
-        for key in list(state_dict.keys()):
-            if key.startswith('ema_model.backbone'):
-                del state_dict[key]
-            elif key.startswith('ema_model.decode_head'):
-                state_dict[key.replace('ema_model.decode_head', 'decode_head')] = state_dict.pop(key)
+        if meta is None:
+            meta = {}
+        elif not isinstance(meta, dict):
+            raise TypeError(
+                f'meta should be a dict or None, but got {type(meta)}')
 
-        # Add comments to describe the usage of `after_load_ckpt`
-        self.call_hook('after_load_checkpoint', checkpoint=checkpoint)
+        if by_epoch:
+            # self.epoch increments 1 after
+            # `self.call_hook('after_train_epoch)` but `save_checkpoint` is
+            # called by `after_train_epoch`` method of `CheckpointHook` so
+            # `epoch` should be `self.epoch + 1`
+            meta.setdefault('epoch', self.epoch + 1)
+            meta.setdefault('iter', self.iter)
+        else:
+            meta.setdefault('epoch', self.epoch)
+            meta.setdefault('iter', self.iter + 1)
+
+        if file_client_args is not None:
+            warnings.warn(
+                '"file_client_args" will be deprecated in future. '
+                'Please use "backend_args" instead', DeprecationWarning)
+            if backend_args is not None:
+                raise ValueError(
+                    '"file_client_args" and "backend_args" cannot be set at '
+                    'the same time.')
+
+            file_client = FileClient.infer_client(file_client_args, out_dir)
+            filepath = file_client.join_path(out_dir, filename)
+        else:
+            filepath = join_path(  # type: ignore
+                out_dir, filename, backend_args=backend_args)
+
+        meta.update(
+            cfg=self.cfg.pretty_text,
+            seed=self.seed,
+            experiment_name=self.experiment_name,
+            time=time.strftime('%Y%m%d_%H%M%S', time.localtime()),
+            mmengine_version=mmengine.__version__ + get_git_hash())
+
+        if hasattr(self.train_dataloader.dataset, 'metainfo'):
+            meta.update(dataset_meta=self.train_dataloader.dataset.metainfo)
 
         if is_model_wrapper(self.model):
             model = self.model.module
         else:
             model = self.model
 
-        checkpoint = _load_checkpoint_to_model(
-            model, checkpoint, strict, revise_keys=revise_keys)
+        checkpoint = {
+            'meta':
+            meta,
+            'state_dict':
+            weights_to_cpu(model.state_dict()),
+            'message_hub':
+            apply_to(self.message_hub.state_dict(),
+                     lambda x: hasattr(x, 'cpu'), lambda x: x.cpu()),
+        }
+        state_dict = checkpoint['state_dict']
+        state_dict = {k: v for k, v in state_dict.items() if 'ema_model' not in k}
+        checkpoint['state_dict'] = state_dict
+        # save optimizer state dict to checkpoint
+        if save_optimizer:
+            if isinstance(self.optim_wrapper, OptimWrapper):
+                checkpoint['optimizer'] = apply_to(
+                    self.optim_wrapper.state_dict(),
+                    lambda x: hasattr(x, 'cpu'), lambda x: x.cpu())
+            else:
+                raise TypeError(
+                    'self.optim_wrapper should be an `OptimWrapper` '
+                    'or `OptimWrapperDict` instance, but got '
+                    f'{self.optim_wrapper}')
 
-        self._has_loaded = True
+        # save param scheduler state dict
+        if save_param_scheduler and self.param_schedulers is None:
+            self.logger.warning(
+                '`save_param_scheduler` is True but `self.param_schedulers` '
+                'is None, so skip saving parameter schedulers')
+            save_param_scheduler = False
+        if save_param_scheduler:
+            if isinstance(self.param_schedulers, dict):
+                checkpoint['param_schedulers'] = dict()
+                for name, schedulers in self.param_schedulers.items():
+                    checkpoint['param_schedulers'][name] = []
+                    for scheduler in schedulers:
+                        state_dict = scheduler.state_dict()
+                        checkpoint['param_schedulers'][name].append(state_dict)
+            else:
+                checkpoint['param_schedulers'] = []
+                for scheduler in self.param_schedulers:  # type: ignore
+                    state_dict = scheduler.state_dict()  # type: ignore
+                    checkpoint['param_schedulers'].append(state_dict)
 
-        self.logger.info(f'Load checkpoint from {filename}')
-
-        return checkpoint
+        self.call_hook('before_save_checkpoint', checkpoint=checkpoint)
+        save_checkpoint(
+            checkpoint,
+            filepath,
+            file_client_args=file_client_args,
+            backend_args=backend_args)
